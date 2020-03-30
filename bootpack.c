abc5d8cdd3688d6f0eba71217492f47f8bcb320e
lyj00912 <3122742760@qq.com>
2020-03-30 19:45:59 +0800

    initialize GDT and IDT

--- bootpack.c
+++ bootpack.c
@@ -35,6 +35,18 @@ struct BOOTINFO{
 	char *VideoRamAddress;
 };
 
+struct SEGMENT_DESCRIPTOR {
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
 int global_cursorX = 8;
 int global_cursorY = 8;
 
@@ -203,6 +215,54 @@ void video_drawBitmap(struct BOOTINFO *binfo, int bitmapWidth, int bitmapHeight,
 	return;
 }
 
+void init_gdtidt(void)
+{
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;
+	int i;
+
+	/* GDT�̏����� */
+	for (i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+	load_gdtr(0xffff, 0x00270000);
+
+	/* IDT�̏����� */
+	for (i = 0; i < 256; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(0x7ff, 0x0026f800);
+
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	if (limit > 0xfffff) {
+		ar |= 0x8000; /* G_bit = 1 */
+		limit /= 0x1000;
+	}
+	sd->limit_low    = limit & 0xffff;
+	sd->base_low     = base & 0xffff;
+	sd->base_mid     = (base >> 16) & 0xff;
+	sd->access_right = ar & 0xff;
+	sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+	sd->base_high    = (base >> 24) & 0xff;
+	return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
+}
+
 
 
 
@@ -215,12 +275,29 @@ void HariMain(void)
 	struct BOOTINFO *binfo;
 	binfo = (struct BOOTINFO *) 0x0ff0;  //从asmhead.nas里读数据
 
+	//初始化GDT和IDT
+	init_gdtidt();
+
+	//初始化调色板
 	video_init_palette();
+
+	//初始化鼠标
+	char mcursor[256];
+	int mx = (binfo->screenWidth - 16) / 2;
+	int my = (binfo->screenHeight - 28 - 16) / 2;
+	video_init_mouse_cursor8(mcursor, COL8_008484);
+
+	//绘制背景
 	video_refreshBackground(binfo->VideoRamAddress, binfo->screenWidth, binfo->screenHeight);
 
+	//绘制鼠标
+	video_drawBitmap(binfo, 16, 16, mx, my, mcursor, 16);
+
+	//测试英文字母
 	video_println(binfo, "A quick brown fox jump over ");
 	video_println(binfo, "the lazy dog.");
 
+	//打印BootInfo
 	char buffer[20];
 	sprintf(buffer, "binfo->cyls = %d", binfo->cyls);
 	video_println(binfo, buffer);
@@ -240,12 +317,7 @@ void HariMain(void)
 	sprintf(buffer, "binfo->VideoRamAddress = 0x%x", binfo->VideoRamAddress);
 	video_println(binfo, buffer);
 
-	char mcursor[256];
-	int mx = (binfo->screenWidth - 16) / 2;
-	int my = (binfo->screenHeight - 28 - 16) / 2;
-	video_init_mouse_cursor8(mcursor, COL8_008484);
-	video_drawBitmap(binfo, 16, 16, mx, my, mcursor, 16);
-
+	//休眠
 	for (;;) {
 		io_hlt();
 	}
